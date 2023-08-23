7ace0e8a34e498cca371d8a382a407dacda9a48b
Ciro de Oliveira <c.dom.oliveira@gmail.com>
2023-08-23 17:16:25 -0300

    duvida

--- app/src/main/java/co/tiagoaguiar/tutorial/jokerappdev/activities/MainActivity.kt
+++ app/src/main/java/co/tiagoaguiar/tutorial/jokerappdev/activities/MainActivity.kt
@@ -4,13 +4,19 @@ import android.app.Activity
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.navigation.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.setupActionBarWithNavController
+import androidx.navigation.ui.setupWithNavController
 import co.tiagoaguiar.tutorial.jokerappdev.R
 import co.tiagoaguiar.tutorial.jokerappdev.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
   private val binding by lazy {
     ActivityMainBinding.inflate(layoutInflater)
   }
+  private val navController = findNavController(R.id.frag_host_main_content)
+  private lateinit var appBarConfig: AppBarConfiguration
 
   override fun onCreate(savedInstanceState: Bundle?) {
     super.onCreate(savedInstanceState)
@@ -21,6 +27,19 @@ class MainActivity : AppCompatActivity() {
 
     val drawerLayout = binding.drawerLayout
     val navView = binding.navView
-    val navController = findNavController(R.id.frag_host_main_content)
+
+    appBarConfig = AppBarConfiguration(
+      setOf(
+        R.id.nav_home, R.id.nav_day_joke, R.id.nav_about
+      ),
+      drawerLayout
+    )
+
+    setupActionBarWithNavController(navController, appBarConfig)
+    navView.setupWithNavController(navController)
+  }
+
+  override fun onSupportNavigateUp(): Boolean {
+    return navController.navigateUp() || super.onSupportNavigateUp()
   }
 }
